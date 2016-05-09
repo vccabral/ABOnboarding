07a754f2c4d26ad19b763a7f1e23cd6bd3e4d061
Adam Boyd <adamboyd50@gmail.com>
2016-05-09 11:31:02 -0700

    getting rid of the hex uicolor extension

--- Pod/Classes/ABOnboardingSettings.swift
+++ Pod/Classes/ABOnboardingSettings.swift
@@ -13,18 +13,18 @@ public struct ABOnboardingSettings {
     //Background and text
     public static var OnboardingBackground = UIColor.whiteColor()
     public static var OnboardingNextButtonBackground = UIColor.whiteColor()
-    public static var OnboardingText = UIColor(rgba: "#9b9b9b")
-    public static var OnboardingLaterText = UIColor(rgba: "#9b9b9b")
+    public static var OnboardingText = UIColor.grayColor()
+    public static var OnboardingLaterText = UIColor.grayColor()
     public static var BackgroundWhileOnboarding = UIColor.blackColor().colorWithAlphaComponent(0.85)
     public static var Font = UIFont.systemFontOfSize(16)
     
     //Rounded button
-    public static var ButtonBackgroundNormal = UIColor(rgba: "#50e3c2")
+    public static var ButtonBackgroundNormal = UIColor.redColor()
     public static var ButtonBackgroundHighlighted = UIColor.clearColor()
     public static var ButtonTextNormal = UIColor.whiteColor()
-    public static var ButtonTextHighlighted = UIColor(rgba: "#9b9b9b")
+    public static var ButtonTextHighlighted = UIColor.grayColor()
     public static var ButtonBorderNormal = UIColor.clearColor()
-    public static var ButtonBorderHighlighted = UIColor(rgba: "#9b9b9b")
+    public static var ButtonBorderHighlighted = UIColor.grayColor()
     
     public static var ViewToShowOnboarding: UIView?
     public static var AnimationDuration: NSTimeInterval = 0.5
